8e2c3c70cb8d36a03df24ef9769be50ef5e66fb1
lj8175 <lj8175@gmail.com>
2013-07-11 21:43:30 +0800

    20130710cppkmp

--- 20130710cppkmp/kmp.cpp
+++ 20130710cppkmp/kmp.cpp
@@ -1,37 +1,68 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
-vector<int> getNext(string str)
+static void getNext(const char* str, int next[])
 {
-    vector<int> v;
     int i=0, j=-1;
-    v.push_back(j);
-    while (i < str.length())
+    const int len=strlen(str);
+    next[i] = j;
+    while (i < len)
     {
         if ((j == -1) || (str[i] == str[j]))
         {
             ++i;
             ++j;
-            v.push_back(j);
+            if (i < len)
+            {
+                next[i] = j;
+            }
         } 
         else
         {
-            j = v[j];
+            j = next[j];
         }
         
     }
-    return v;
+}
+
+int kmpIndex(const char* str, const char* search, int pos=0)
+{
+    int i=pos, j=0;
+    const int strLen = strlen(str);
+    const int searchLen = strlen(search);
+    int next[searchLen];
+    getNext(search, next);
+    while ((i<strLen) && (j<searchLen))
+    {
+        if ((j==-1) || (str[i]==search[j]))
+        {
+            ++i;
+            ++j;
+        }
+        else
+        {
+            j = next[j];
+        }
+    }
+
+    if (j>=searchLen)
+    {
+        return i-searchLen;
+    }
+    else
+    {
+        return -1;
+    }
 
 }
 
 int main()
 {
-    vector<int> v = getNext(string("abaabcac"));
-    for (int i=0; i<v.size(); ++i)
-    {
-        cout << v[i] << endl;
-    }
+    char *str = (char*)"acabaabaabcacaabc";
+    char *search= (char*)"abaabcac";
+    cout << kmpIndex(str, search) << endl;
 }
