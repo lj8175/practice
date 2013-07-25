3aa7781b16da6f725312f4a077067ac93ed68cac
lj8175 <lj8175@gmail.com>
2013-07-25 20:42:35 +0800

    20130725cppAesCfb

--- 20130725cppAesCfb/aescfb.cpp
+++ 20130725cppAesCfb/aescfb.cpp
@@ -33,7 +33,7 @@ int AES_Decrypt(const char *key, const char *str, int strLen, char* deStr)
     unsigned char iv[AES_BLOCK_SIZE];
     bzero(iv, AES_BLOCK_SIZE);
     AES_KEY aes;
-    if (AES_set_decrypt_key((unsigned char*)key, 128, &aes) < 0)
+    if (AES_set_encrypt_key((unsigned char*)key, 128, &aes) < 0)
     {
         return -1;
     }
@@ -56,18 +56,20 @@ void hex_print(char* str, int size)
 int main(int argc, char** argv)
 {
     char *source=(char*)"1234567890123456";
+    const int len = strlen(source);
     if (argc>1)
     {
         source = argv[1];
     }
     int ret = 0;
-    char ptr_en[BUFFER_SIZE],ptr_de[BUFFER_SIZE],aes_en[BUFFER_SIZE],aes_de[BUFFER_SIZE];
+    char aes_en[BUFFER_SIZE]={0};
+    char aes_de[BUFFER_SIZE]={0};
 
-    int aes_len=0;
-    ret = AES_Encrypt(source,source,strlen(source),aes_en); CHECK_RET(ret);
-    hex_print(aes_en, strlen(source));
-    ret = AES_Decrypt(source,aes_en,strlen(source),aes_de); CHECK_RET(ret);
-    printf("len: %d, decrypt is  : %s\n", strlen(aes_de), aes_de);
     printf("source is  : %s\n", source);
+    ret = AES_Encrypt(source,source,len,aes_en); CHECK_RET(ret);
+    hex_print(aes_en, strlen(source));
+    ret = AES_Decrypt(source,aes_en,len,aes_de); CHECK_RET(ret);
+    aes_de[len] = '\0';
+    printf("size: %d, decrypt is  : %s\n", len, aes_de);
     return 0;
 }
