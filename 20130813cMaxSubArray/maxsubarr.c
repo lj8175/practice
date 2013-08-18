724b8f995633cc9dd962b670c0616a7d0b2e08cf
lj8175 <lj8175@gmail.com>
2013-08-18 16:12:35 +0800

    20130813cMaxSubArray dynamic

--- 20130813cMaxSubArray/maxsubarr.c
+++ 20130813cMaxSubArray/maxsubarr.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define MAX(x,y) ((x)>(y)?(x):(y))
+
 int maxSumSubArr(int arr[], int* begin, int* end)
 {
 	int max = INT32_MIN, sum = -1, pres, s, e;
@@ -25,16 +27,30 @@ int maxSumSubArr(int arr[], int* begin, int* end)
 	return max;
 }
 
+int MaxSum(int *arr, int n)
+{
+	int Start[n], All[n];
+	Start[n-1] = arr[n-1];
+	All[n-1]=arr[n-1];
+	for(int i=n-2;i>=0;i--)
+	{
+		Start[i] = MAX(arr[i], arr[i]+Start[i+1]);
+		All[i] = MAX(Start[i], All[i+1]);
+	}
+	return All[0];
+}
+
 int main()
 {
-	//int arr[] = {3, 15, -1, 6, -8, 9, 7,-6};
+	int arr[] = {3, 15, -1, 6, -8, 9, 7,-6};
 	//int arr[] = {1, -2, 3, 10, -4, 7, 2, -5};
 	//int arr[] = {-1, -2, -3, -4};
 	//int arr[] = {-4,-3,-2,-1};
-	int arr[] = {-4,-1,-2,-3};
+	//int arr[] = {-4,-1,-2,-3};
 	int begin=0;
 	int end = sizeof(arr)/sizeof(int);
 	int sum = maxSumSubArr(arr, &begin, &end);
-	printf("sum[%d], begin[%d], end[%d]\n",sum, begin, end);
+	int sum1 = MaxSum(arr,sizeof(arr)/sizeof(int));
+	printf("sum[%d], sum1[%d], begin[%d], end[%d]\n",sum, sum1, begin, end);
 	
 }
